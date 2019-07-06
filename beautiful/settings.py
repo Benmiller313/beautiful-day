cea1e2a0688637efb49659d730111173ade00778
benmiller313 <benmiller313@gmail.com>
2019-07-06 12:51:41 -0400

    force https

--- beautiful/settings.py
+++ beautiful/settings.py
@@ -75,7 +75,7 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'beautiful.wsgi.application'
-
+SECURE_SSL_REDIRECT = not DEBUG
 
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases

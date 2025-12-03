95fa4f0e9b8371ea8f8b7dfb5a372989b97d6f39
Ben Miller <benmiller313@gmail.com>
2025-12-03 15:35:45 -0800

    fix whitenoise

--- beautiful/settings.py
+++ beautiful/settings.py
@@ -134,5 +134,5 @@ STATICFILES_DIRS = [
 ]
 
 
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
